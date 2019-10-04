eb6ff27bbd35539018e03f7684854d3ee4ca5801
ballerr897 <55497919+ballerr897@users.noreply.github.com>
2019-10-04 10:30:36 -0400

    Add files via upload

--- 117keagan.py
+++ 117keagan.py
@@ -1,26 +1,61 @@
-#   a117_grades.py
-#   This code is incomplete.
-my_courses = ["English", "Math", "CS"]
+#  a117_turtles_in_traffic.py
+#   Move turtles horizontally and vertically across screen.
+#   Stopping turtles when they collide.
+import turtle as trtl
 
-check_grades = True
-while (check_grades):
-    for course in my_courses:
-        print() # blank line
-        print("Enter your points for " + course)
+# create two empty lists of turtles, adding to them later
+horiz_turtles = []
+vert_turtles = []
 
-        a = int(input("Points -> "))
-        if (a >= 90):
-	     print("A")
-        elif (a >= 80):
-	    print("B")
-        elif (a >= 70):
-	    print("C")
-        elif (a >= 60):
-	    print("D")
-        else:
-	    print("F")
-        redo = input("Do you need to re-do these grades? (y/n)")
-        if (redo == "y"):
-        check_grades = True
-        else:
-        check_grades = False
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+
+tloc = 50
+for s in turtle_shapes:
+
+  ht = trtl.Turtle(shape=s)
+  horiz_turtles.append(ht)
+  ht.penup()
+  new_color = horiz_colors.pop()
+  ht.fillcolor(new_color)
+  ht.goto(-350, tloc)
+  ht.setheading(0)
+
+  vt = trtl.Turtle(shape=s)
+  vert_turtles.append(vt)
+  vt.penup()
+  new_color = vert_colors.pop()
+  vt.fillcolor(new_color)
+  vt.goto( -tloc, 350)
+  vt.setheading(270)
+  
+  tloc += 50
+
+# TODO: move turtles across and down screen, stopping for collisions
+
+steps = 0
+while steps < 50:
+  steps = steps + 1
+
+  for h_turtle in horiz_turtles:
+      for v_turtle in vert_turtles:
+
+          v_turtle.forward(10)
+          h_turtle.forward(10)
+
+          x1 = v_turtle.xcor()
+          x2 = h_turtle.xcor()
+
+          y1 = v_turtle.ycor()
+          y2 = h_turtle.ycor()
+
+          if abs(x1 -x2) < 20 and abs(y1 - y2) < 20:
+              v_turtle.fillcolor("gray")
+              h_turtle.fillcolor("gray")
+
+              horiz_turtles.remove(h_turtle)
+              vert_turtles.remove(v_turtle)
+wn = trtl.Screen()
+wn.mainloop()
